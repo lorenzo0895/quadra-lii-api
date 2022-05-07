e226c752619749f636daa5801a16317a95abf84f
Lorenzo Spallione <lorenzo0895@gmail.com>
2022-05-07 12:41:34 -0300

    cors added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
+var cors = require('cors')
 
 const uriDB = 'mongodb+srv://root:root@cluster0.wsldw.mongodb.net/quadralii?retryWrites=true&w=majority';
 mongoose.connect(uriDB)
@@ -9,6 +10,7 @@ mongoose.connect(uriDB)
   .catch('Error al conectarse a la BD');
 
 app.use(express.json());
+app.use(cors())
 
 app.use('/books', require('./routes/booksRoute'));
 app.use('/flats', require('./routes/flatsRoute'));
